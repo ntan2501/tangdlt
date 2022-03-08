929e4b94d4925946e9cc288251cf02f996d4754a
ntan2501 <99628805+ntan2501@users.noreply.github.com>
2022-03-08 18:51:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Em';
+const nameGirl = 'Ngừi yêu tôi';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 8/3';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc bé iu của anh 8/3 có thật nhiều niềm vui và hạnh phúc (1 lần nữa:>), xin lũi em vì chút sơ suất nhỏ của anh :> Mong phần quà bé xíu xiu này có thể khiến cho ngày hôm nay của em trở nên đẹp hơn và lúc nào cũng tươi tắn như trong hình :3. ';
+const titleCard = 'Tặng em';
+const contentCard = 'Chúc bé iu của anh 8/3 có thật nhiều niềm vui và hạnh phúc (1 lần nữa:>), xin lũi em vì chút sơ suất nhỏ của anh :> Mong phần quà bé xíu xiu này có thể khiến cho ngày hôm nay của em trở nên đẹp hơn và lúc nào cũng tươi tắn như trong hình :3. You are the apple of my eye <3';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
